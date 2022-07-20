e8264d54b2a551bf1f3074477cb7a116aae47d4c
欧阳鹏 <61752998+oyps@users.noreply.github.com>
2022-07-20 20:38:46 +0800

    Update get_title.php

--- api/get_title.php
+++ api/get_title.php
@@ -8,13 +8,53 @@ require 'Poncon.php';
 use Poncon\Poncon;
 
 $poncon = new Poncon();
-
 $url = $poncon->POST('url', null, true);
+
+$input = json_decode('{
+  "options": {
+    "http": {
+      "method": "GET",
+      "header": "Content-type: application/x-www-form-urlencoded\n",
+      "content": "",
+      "timeout": 900,
+      "proxy": "tcp://localhost:7890",
+      "request_fulluri": true
+    },
+    "ssl": {
+      "verify_peer": false,
+      "verify_peer_name": false
+    }
+  },
+  "url": "' . $url . '",
+  "charset": "utf-8"
+}', true);
+
+// $http是个数组，前端需要传入一个JSON
+$options = $input['options'];
+$charset = $input['charset'];
+if ($charset != 'utf-8') {
+    $header = $options['http']['header'];
+    $options['http']['header'] = iconv('utf-8', $charset, $header);
+}
+$context = stream_context_create($options);
+$ym = file_get_contents($input['url'], false, $context);
+foreach ($http_response_header as $key => $value) {
+    $item = explode(':', $value);
+    if (strtolower($item[0]) == 'content-encoding' && strtolower($item[1]) == ' gzip') {
+        $result = gzdecode($result);
+    }
+}
+
+if ($charset != 'utf-8') {
+    $result = iconv($charset, 'utf-8//IGNORE', $result);
+}
+
+
+
 if (!$url) {
     $poncon->error(900, '参数缺失');
 }
 
-$ym = file_get_contents($url);
 
 $title = $poncon->sj($ym, '<title', '</title>');
 $title = $poncon->sj($title, '>', null);
